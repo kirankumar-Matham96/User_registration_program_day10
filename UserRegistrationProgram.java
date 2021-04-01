c8d6e19f3b08726a69c9b6756b3c3c21b503e653
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-04-01 20:58:51 +0530

    ADD: Vlaidating phone number of the user

--- UserRegistrationProgram.java
+++ UserRegistrationProgram.java
@@ -36,13 +36,27 @@ public class UserRegistrationProgram
 			return "invalid";
 	}
 
+	/*
+	 * UC-4: validating predefined mobile number
+	 */
+	public static String validatePhoneNumber(String phoneNumber)
+	{
+		if(Pattern.matches("^[91]{2}[0-9]{10}",phoneNumber))
+			return "valid";
+		else
+			return "invalid";
+	}
+
 	//main method
 	public static void main(String[] args)
 	{
 		String firstName = "Rahul";
 		String lastName = "Shinde";
+		long phoneNumber = 918688332960L;
+
 		System.out.println("first name: "+validateFirstName(firstName));
 		System.out.println("last name: "+validateLastName(lastName));
+		System.out.println("phone numbar: "+validatePhoneNumber(Long.toString(phoneNumber)));
 		System.out.println("Rahul@gmail.com: "+validateEmail("Rahul@gmail.com"));
 	}
 }
