7280bff34d2dd80d4aed81822803026da8b9ac08
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-04-01 20:40:56 +0530

    ADD: Validating first name of the user

--- UserRegistrationProgram.java
+++ UserRegistrationProgram.java
@@ -1,8 +1,24 @@
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+
 public class UserRegistrationProgram
 {
+	/*
+	 * UC-1: validating first name
+	 */
+	public static String validateFirstName(String firstName)
+	{
+		if(Pattern.matches("^[A-Z]{1}[a-z]{2,}", firstName))
+			return "valid";
+		else
+			return "invalid";
+	}
+
 	//main method
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to user registartion program!");
+		String firstName = "Rahul";
+		System.out.println("first name: "+validateFirstName(firstName));
 	}
 }
